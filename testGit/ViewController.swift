33138f21ae34d1ebf9af30189f1964a3dab1b5bd
victory1908 <victory1908@gmail.com>
2016-03-17 15:13:45 +0800

    create branch

--- testGit/ViewController.swift
+++ testGit/ViewController.swift
@@ -13,9 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        print ("test")
-        
     }
 
     override func didReceiveMemoryWarning() {
